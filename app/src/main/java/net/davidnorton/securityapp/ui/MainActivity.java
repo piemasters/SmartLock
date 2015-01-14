ff2902c8cbb26a38f91eea933d04bbce34b74835
piemasters <pie_masters@hotmail.co.uk>
2015-01-14 14:05:56 +0000

    Notification opens profile page.

--- app/src/main/java/net/davidnorton/securityapp/ui/MainActivity.java
+++ app/src/main/java/net/davidnorton/securityapp/ui/MainActivity.java
@@ -11,6 +11,7 @@ import android.app.FragmentManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -98,9 +99,14 @@ public class MainActivity extends Activity {
         if (pref.getBoolean("notification", true)) {
             permNotification = true;
 
-            Intent resultIntent = new Intent(this, ListDialogActivity.class);
-            PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+            // Set activity to run when notification is touched
+            Intent resultIntent = new Intent(this, MainActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
+            // Build notification
             NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
 
             nBuilder.setSmallIcon(R.drawable.notification_icon);
